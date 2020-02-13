dfd5ae9a8310dc07330ddd6cfd74394728b179b5
Jauhar Arifin <jauhar.arifin@shopee.com>
2020-02-13 23:22:17 +0800

    add go:generate command example

--- examples/add.go
+++ examples/add.go
@@ -2,4 +2,6 @@ package examples
 
 import "context"
 
+//go:generate gomocker gen AddFunc --package github.com/jauhararifin/gomocker/examples --target-package examples --output add_mock.go
+
 type AddFunc func(ctx context.Context, a, b int) (sum int, err error)
--- examples/math.go
+++ examples/math.go
@@ -2,6 +2,8 @@ package examples
 
 import "context"
 
+//go:generate gomocker gen Math --package github.com/jauhararifin/gomocker/examples --target-package examples --output math_mock.go
+
 type Math interface {
 	Add(ctx context.Context, a, b int) (sum int, err error)
 	Subtract(ctx context.Context, a, b int) (result int, err error)
